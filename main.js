3c098fee4cedd9f3edfe468a2a2e7e9d3c1a72d9
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-06-14 16:56:02 +0530

    simultanious actions at a time using axios.all()

--- main.js
+++ main.js
@@ -57,7 +57,18 @@ function removeTodo() {
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  //console.log('Simultaneous Request');
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  ])
+    .then((res)=>{
+      console.log(res[0]);
+      console.log(res[1]);
+      //showOutput(res[0]);
+      showOutput(res[1]);
+    })
+    .catch(err=>console.log(err));
 }
 
 // CUSTOM HEADERS
